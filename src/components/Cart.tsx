8c907418bd153a62f98867a3a8a891e1addffdd1
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2021-09-15 18:53:07 +0600

    initial

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,28 +1,36 @@
-import {ICartItemType} from '../type'
-import {FunctionComponent} from 'react'
-import CartItems from './CartItems'
+import { ICartItemType } from '../type';
+import { FunctionComponent } from 'react';
+import CartItems from './CartItems';
 
-type Props ={
-  cartItems:ICartItemType[],
-  addToCart:(clickItem: ICartItemType)=>void,
-  removeFromCart:(id:number)=>void,
-}
+type Props = {
+  cartItems: ICartItemType[];
+  addToCart: (clickItem: ICartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
 
-
-const Cart:FunctionComponent<Props> = ({addToCart,cartItems,removeFromCart}) => {
-  const calculatorTotal = (items:ICartItemType[]) => {
-    items.reduce((prev:number, item) => prev + item.amount * item.price, 0);
- }
+const Cart: FunctionComponent<Props> = ({
+  addToCart,
+  cartItems,
+  removeFromCart,
+}) => {
+  const calculatorTotal = (items: ICartItemType[]) => {
+    items.reduce((prev: number, item) => prev + item.amount * item.price, 0);
+  };
   return (
-    <div className="w-[500px] p-5">
+    <div className='w-[500px] p-5'>
       <h2>Your shopping Cart</h2>
-      {cartItems.length === 0?<p>No items in cart</p>:null}
-      {cartItems.map(item =>(
-        <CartItems item={item} key={item.id} addToCart={addToCart} removeFromCart={removeFromCart}/>
+      {cartItems.length === 0 ? <p>No items in cart</p> : null}
+      {cartItems.map(item => (
+        <CartItems
+          item={item}
+          key={item.id}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
       ))}
-       <h1>Total: ${calculatorTotal(cartItems)}</h1>
+      <h1>Total: ${calculatorTotal(cartItems)}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
